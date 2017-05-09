b4d2c80e35df895f87535eebef9528f4d902f0bf
vnthanh25 <vnthanh25@gmail.com>
2017-05-09 20:55:21 +0700

    Buoi 6

---
+++ WebContent/js/index.js
@@ -0,0 +1,25 @@
+
+var app = angular.module('app', []);
+
+// define controller
+app.controller('indexController', function($scope) {
+	$scope.hello = 'Hello world!';
+	
+	var sub = ['Tư liệu', 'Phân tích', 'Người Việt 5 châu', 'Cuộc sống đó đây', 'Quân sự'];
+	
+	var thegioi = {id: 1, name: 'Thế giới', children: sub};
+	var thethao = {id: 2, name: 'Thể thao', children: []};
+	var phapluat = {id: 3, name: 'Pháp luật', children: []};
+	var khoahoc = {id: 4, name: 'Khoa học', children: []};
+	
+	
+	// create array.
+	$scope.menus = [thegioi, thethao, phapluat, khoahoc];
+	
+	$scope.menus1 = [thegioi, thethao];
+	// add more.
+	$scope.menus1.push(phapluat);
+	$scope.menus1.push(khoahoc);
+	
+});
+
